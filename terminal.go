1019fdf9c22025466ad03a42b455ff51faa471f2
jagottsicher <jagottsicher@gmx.net>
2022-10-07 16:52:22 +0800

    added os-independent screen clearing

---
+++ terminal.go
@@ -0,0 +1,21 @@
+// myGoToolbox is package which contains a collection of useful functions and handy tools for recurring tasks in your Go projects
+package myGoToolbox
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+// Clearscreen empties the terminal screen independently from the underlying operation system
+func Clearscreen() {
+	if runtime.GOOS != "windows" {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
